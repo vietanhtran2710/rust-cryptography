68373c08d209b5a39ab9799b698e1ca01546bbce
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-08-15 21:40:53 +0700

    add affine cipher decryption

--- src/affine.rs
+++ src/affine.rs
@@ -26,10 +26,39 @@ pub fn check_key(a: u16, b: u16) -> bool {
         if a == 1 && b == 0 { return false; }
     }
     return true;
-} 
+}
+
+fn get_inverse(a: u16) -> i32 {
+    let (mut r_i1, mut r_i) = (a as i32, 26 as i32);
+    let mut remainder = 1;
+    let (mut s_i1, mut s_i) = (1, 0);
+    while remainder != 0 {
+        remainder = r_i1 % r_i;
+        let q = r_i1 / r_i;
+        let new_s = s_i1 - q * s_i;
+        r_i1 = r_i; r_i = remainder;
+        s_i1 = s_i; s_i = new_s;    
+    }
+    s_i1
+}
 
 pub fn decrypt(input: String, key_a: u16, key_b: u16) -> String {
-    "".to_string()
+    let a_1 = get_inverse(key_a);
+    input.chars().map(|c| 
+        if c.is_ascii_lowercase() {
+            let index = ASCII_LOWER.iter().position(|&r| r == c).unwrap();
+            let new_index: i32 = (a_1 * (index as i32 - key_b as i32)).rem_euclid(26);
+            ASCII_LOWER[new_index as usize]
+        }
+        else if c.is_ascii_uppercase() {
+            let index = ASCII_UPPER.iter().position(|&r| r == c).unwrap();
+            let new_index = (a_1 * (index as i32 - key_b as i32)).rem_euclid(26);
+            ASCII_UPPER[new_index as usize]
+        }
+        else {
+            c
+        }
+    ).collect()
 }
 
 pub fn encrypt(input: String, key_a: u16, key_b: u16) -> String {
